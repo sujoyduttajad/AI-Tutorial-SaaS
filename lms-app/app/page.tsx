9d94be306096efe34b4f9f06af70935fff982763
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2025-09-13 15:22:19 -0600

    reinstall Next.js with ShadCN

---
+++ lms-app/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="text-2xl underline">Welcome to my SaaS app</h1>
+      <Button>Press me</Button>
+   </div>
+  );
+}
